00b2d2e51f2a5a1ce69df982751daf99a765f358
David Freerksen <dfreerksen@gmail.com>
2016-03-31 11:08:52 -0700

    Use tap over each_with_object

--- app/helpers/admin/base_helper_decorator.rb
+++ app/helpers/admin/base_helper_decorator.rb
@@ -1,7 +1,9 @@
 Spree::Admin::BaseHelper.class_eval do
   def package_type_options
-    SpreePackages::TYPES.each_with_object([]) do |type, opt|
-      opt << [Spree.t("packages.type.#{type}"), type]
+    [].tap do |obj|
+      SpreePackages::TYPES.each do |type|
+        obj << [Spree.t("packages.type.#{type}"), type]
+      end
     end
   end
 end
