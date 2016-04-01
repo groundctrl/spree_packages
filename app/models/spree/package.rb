09b64f9adb264174b092de9514c8ad467e850029
David Freerksen <dfreerksen@gmail.com>
2016-04-01 16:46:21 -0700

    Use acts_as_paranoid for packages

--- app/models/spree/package.rb
+++ app/models/spree/package.rb
@@ -1,5 +1,9 @@
 module Spree
   class Package < ActiveRecord::Base
+    acts_as_paranoid
+
+    after_destroy :column_reset
+
     validates :depth, numericality: { greater_than: 0 }
     validates :height, numericality: { greater_than: 0 }
     validates :name, presence: true, uniqueness: true
@@ -8,5 +12,11 @@ module Spree
     validates :width, numericality: { greater_than: 0 }
 
     default_scope { order(name: :asc) }
+
+    protected
+
+    def column_reset
+      update_attributes(name: "#{Time.current.to_i}_#{name}")
+    end
   end
 end
